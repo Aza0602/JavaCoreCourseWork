13df602a992f67d044dd0864da1480f98f6a9241
Azat <aelchikushuulu@gmail.com>
2023-05-14 16:38:36 +0600

    Initial commit

---
+++ src/main/java/com/github/aza06_02/javacorecoursework/JavaCoreCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package com.github.aza06_02.javacorecoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreCourseWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCoreCourseWorkApplication.class, args);
+    }
+
+}
